6fc7f68c6f38682471d110b82873d79d7c9e956d
Arpan Laha <arpanlaha99@gmail.com>
2019-07-28 18:20:22 -0700

    use correct props

--- components/Head.tsx
+++ components/Head.tsx
@@ -4,7 +4,7 @@ import NextHead from "next/head";
 interface HeadProps extends Props<Component> {
   title?: string;
   description?: string;
-  keywords?: string[];
+  keywords?: string;
 }
 
 class Head extends Component<HeadProps> {
@@ -13,7 +13,7 @@ class Head extends Component<HeadProps> {
       <NextHead>
         <meta charSet="UTF-8" />
         <meta name="description" content={this.props.description || ""} />
-        <meta name="keywords" content={this.props.description || ""} />
+        <meta name="keywords" content={this.props.keywords || ""} />
         <meta name="author" content="Arpan Laha" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="language" content="English" />
